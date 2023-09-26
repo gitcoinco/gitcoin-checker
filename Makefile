e8a718a4c0ec9f17343ef783ce50147af2a968ab
Gerrit Giliomee <giliomeejg@gmail.com>
2023-09-26 17:36:34 +0200

    func(Initial Laravel / Vue setup):

---
+++ Makefile
@@ -0,0 +1,13 @@
+.PHONY: up
+up:
+	./vendor/bin/sail up
+
+.PHONY: down
+down:
+	./vendor/bin/sail down
+
+
+.PHONY: in
+in:
+	docker exec -it checker-laravel.test-1 /bin/bash
+
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,126 @@
+<script setup>
+import { Head, Link, useForm } from "@inertiajs/vue3";
+import AuthenticationCard from "@/Components/AuthenticationCard.vue";
+import AuthenticationCardLogo from "@/Components/AuthenticationCardLogo.vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import Web3 from "web3";
+
+defineProps({
+    canResetPassword: Boolean,
+    status: String,
+});
+
+const form = useForm({
+    email: "",
+    password: "",
+    remember: false,
+});
+
+const submit = () => {
+    form.transform((data) => ({
+        ...data,
+        remember: form.remember ? "on" : "",
+    })).post(route("login"), {
+        onFinish: () => form.reset("password"),
+    });
+};
+
+const loginWeb3 = async () => {
+    if (!window.ethereum) {
+        alert(
+            "MetaMask not detected. Please try again from a MetaMask enabled browser."
+        );
+    }
+
+    const web3 = new Web3(window.ethereum);
+
+    const message = [
+        "I have read and accept the terms and conditions (https://example.org/tos) of this app.",
+        "Please sign me in!",
+    ].join("\n");
+
+    const address = (await web3.eth.requestAccounts())[0];
+    const signature = await web3.eth.personal.sign(message, address);
+
+    return useForm({ message, address, signature }).post("/login-web3");
+};
+</script>
+
+<template>
+    <Head title="Log in" />
+
+    <AuthenticationCard>
+        <template #logo>
+            <AuthenticationCardLogo />
+        </template>
+
+        <div class="text-center pt-4 pb-8 border-b border-gray-200">
+            <jet-button @click="loginWeb3"> Login with MetaMask </jet-button>
+        </div>
+        <div class="py-6 text-sm text-gray-500 text-center">
+            or login with your credentials…
+        </div>
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="email" value="Email" />
+                <TextInput
+                    id="email"
+                    v-model="form.email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    required
+                    autofocus
+                    autocomplete="username"
+                />
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+                <TextInput
+                    id="password"
+                    v-model="form.password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    required
+                    autocomplete="current-password"
+                />
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <Checkbox v-model:checked="form.remember" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link
+                    v-if="canResetPassword"
+                    :href="route('password.request')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Forgot your password?
+                </Link>
+
+                <PrimaryButton
+                    class="ml-4"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                >
+                    Log in
+                </PrimaryButton>
+            </div>
+        </form>
+    </AuthenticationCard>
+</template>
