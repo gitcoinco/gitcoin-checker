4d555a631569f8dfe80c6e71345750d5e8723681
Gerrit Giliomee <giliomeejg@gmail.com>
2023-10-10 15:31:12 +0200

    func(Telescope): Add to inspect why we're getting a 1s load time on first request on prod.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('ingest:data')->hourly();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
