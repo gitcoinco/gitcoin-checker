2da0636f88f372e77aa1762a1c847337a74d7332
Gerrit Giliomee <giliomeejg@gmail.com>
2023-10-10 13:55:15 +0200

    chore(Text): Small tweaks to dashboard text.

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -22,6 +22,94 @@ import { Link } from "@inertiajs/vue3";
             <div>
                 <div class="flex items-center">
                     <i
+                        class="fa fa-building-o text-gray-400 fa-lg"
+                        aria-hidden="true"
+                    ></i>
+
+                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                        <Link
+                            :href="route('project.index')"
+                            class="text-blue-500 hover:underline"
+                        >
+                            Projects
+                        </Link>
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    A list of projects, ordered by the project with the latest
+                    application submission to a round.
+                </p>
+            </div>
+
+            <div>
+                <div class="flex items-center">
+                    <i
+                        class="fa fa-circle-o text-gray-400 fa-lg"
+                        aria-hidden="true"
+                    ></i>
+                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                        <Link
+                            :href="route('round.index')"
+                            class="text-blue-500 hover:underline"
+                        >
+                            Rounds</Link
+                        >
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    A list of rounds, ordered by the round that has received the
+                    most recent application.
+                </p>
+
+                <p class="mt-4 text-sm">
+                    <Link
+                        :href="route('round.index')"
+                        class="text-blue-500 hover:underline"
+                    >
+                        <span class="flex items-center">
+                            Rounds
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                class="ml-1 w-5 h-5 fill-indigo-500"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </Link>
+                </p>
+            </div>
+
+            <div>
+                <div class="flex items-center">
+                    <i
+                        class="fa fa-cogs text-gray-400 fa-lg"
+                        aria-hidden="true"
+                    ></i>
+
+                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                        <Link
+                            :href="route('round.application.index')"
+                            class="text-blue-500 hover:underline"
+                        >
+                            Applications
+                        </Link>
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    A list of applications ordered by the application date.
+                </p>
+            </div>
+            <div>
+                <div class="flex items-center">
+                    <i
                         class="fa fa-lock text-gray-400 fa-lg"
                         aria-hidden="true"
                     ></i>
@@ -76,95 +164,6 @@ import { Link } from "@inertiajs/vue3";
                         </svg>
                     </Link>
                 </div>
-            </div>
-
-            <div>
-                <div class="flex items-center">
-                    <i
-                        class="fa fa-circle-o text-gray-400 fa-lg"
-                        aria-hidden="true"
-                    ></i>
-                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                        <Link
-                            :href="route('round.index')"
-                            class="text-blue-500 hover:underline"
-                        >
-                            Rounds</Link
-                        >
-                    </h2>
-                </div>
-
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    See a list of rounds.
-                </p>
-
-                <p class="mt-4 text-sm">
-                    <Link
-                        :href="route('round.index')"
-                        class="text-blue-500 hover:underline"
-                    >
-                        <span class="flex items-center">
-                            Rounds
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                class="ml-1 w-5 h-5 fill-indigo-500"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </span>
-                    </Link>
-                </p>
-            </div>
-
-            <div>
-                <div class="flex items-center">
-                    <i
-                        class="fa fa-building-o text-gray-400 fa-lg"
-                        aria-hidden="true"
-                    ></i>
-
-                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                        <Link
-                            :href="route('project.index')"
-                            class="text-blue-500 hover:underline"
-                        >
-                            Projects
-                        </Link>
-                    </h2>
-                </div>
-
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    List of projects.
-                </p>
-            </div>
-
-            <!-- round.application.index -->
-
-            <div>
-                <div class="flex items-center">
-                    <i
-                        class="fa fa-cogs text-gray-400 fa-lg"
-                        aria-hidden="true"
-                    ></i>
-
-                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                        <Link
-                            :href="route('round.application.index')"
-                            class="text-blue-500 hover:underline"
-                        >
-                            Applications
-                        </Link>
-                    </h2>
-                </div>
-
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    List of applications.
-                </p>
             </div>
         </div>
     </div>
