69c9eff18ed858545d7a748c9e612464b3d11348
Gerrit Giliomee <giliomeejg@gmail.com>
2023-11-23 13:35:54 +0200

    chore(Docker): Ensure our laravel user exists.

--- docker/mysql/create-testing-database.sh
+++ docker/mysql/create-testing-database.sh
@@ -2,5 +2,6 @@
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS testing;
+    CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%';
     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
