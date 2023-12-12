6693182863369b45dcd969a5fde60c3b7ffa6095
Gerrit Giliomee <giliomeejg@gmail.com>
2023-12-12 15:57:33 +0200

    fix(Public): Project search.

--- resources/views/public/project/list.blade.php
+++ resources/views/public/project/list.blade.php
@@ -13,7 +13,7 @@ Gitcoin: A list of projects that have been funded by Gitcoin Grants
                 <div class="container py-3">
                     <form action="/public/projects/list">
                         <div class="input-group mb-3">
-                            <input name="query" type="text" value="{{ $search }}" class="form-control" placeholder="Search projects..." aria-label="Search for projects" aria-describedby="button-addon2">
+                            <input name="search" type="text" value="{{ $search }}" class="form-control" placeholder="Search projects..." aria-label="Search for projects" aria-describedby="button-addon2">
                             <div class="input-group-append">
                                 <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
                             </div>
