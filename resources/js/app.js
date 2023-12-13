f6d55733d179610efccb257aa06f3fba5959137b
Gerrit Giliomee <giliomeejg@gmail.com>
2023-12-13 16:51:41 +0200

    fix(Analytics): Routers wasn't defined.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import "./bootstrap";
 import "../css/app.css";
 
 import { createApp, h } from "vue";
-import { createInertiaApp } from "@inertiajs/vue3";
+import { createInertiaApp, router } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 import moment from "moment";
