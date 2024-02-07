b9e00de0f4679c19ff17267cd115d5b8095c7f33
Gerrit Giliomee <giliomeejg@gmail.com>
2024-02-07 17:51:38 +0200

    chore(Notifications): Add Beautymail provider.

--- config/app.php
+++ config/app.php
@@ -172,6 +172,8 @@ return [
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+
+        Snowfire\Beautymail\BeautymailServiceProvider::class,
     ])->toArray(),
 
     /*
