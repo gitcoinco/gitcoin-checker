dc4eaeaeb274acfce07f7b480e2c46f15af37c2a
Gerrit Giliomee <giliomeejg@gmail.com>
2024-02-09 13:32:44 +0200

    chore(Notifications): Hourly notifications not coming through as expected, presumably because the longer running ingest:data might influence the triggering of notifications on the hour.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,8 +9,8 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('ingest:data')->hourly()->withoutOverlapping();
         $schedule->command('app:notifications')->everyMinute()->withoutOverlapping();
+        $schedule->command('ingest:data')->hourly()->withoutOverlapping();
 
         // Regular maintenance command, safe to run daily
         $schedule->command('telescope:prune')->daily();
