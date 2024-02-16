2f4f89adc5136e8f62d81c0e146e53b1598e292e
Gerrit Giliomee <giliomeejg@gmail.com>
2024-02-16 14:23:13 +0200

    chore(GPT): Some data is coming back as null, so let's cater for it.

---
+++ database/migrations/2024_02_16_122210_change_gpt_evaluation_cols_to_null.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('gpt_round_eligibility_scores', function (Blueprint $table) {
+            $table->string('score')->nullable()->change();
+            $table->string('reason')->nullable()->change();
+            $table->string('response')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('gpt_round_eligibility_scores', function (Blueprint $table) {
+            $table->string('score')->nullable(false)->change();
+            $table->string('reason')->nullable(false)->change();
+            $table->string('response')->nullable(false)->change();
+        });
+    }
+};
