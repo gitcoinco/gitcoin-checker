21434a033d2bc112a227f6a4c9a58fe1902b25e5
Gerrit Giliomee <giliomeejg@gmail.com>
2024-02-19 07:28:04 +0200

    func(Evaluation): Highlight evaluations with a high number of uncertainty.

--- resources/js/Pages/Application/Components/ResultsSummary.vue
+++ resources/js/Pages/Application/Components/ResultsSummary.vue
@@ -8,13 +8,15 @@ const props = defineProps({
     },
 });
 
-const gptNrYes = () => {
+const gptNrs = () => {
     if (props.application?.results?.length === 0) {
         return null;
     }
 
     let ret = {
         nrYes: 0,
+        nrNo: 0,
+        nrUncertain: 0,
         totalNrAnswers: 0,
     };
 
@@ -26,6 +28,10 @@ const gptNrYes = () => {
 
             if (results[i].score === "Yes") {
                 ret.nrYes += 1;
+            } else if (results[i].score === "No") {
+                ret.nrNo += 1;
+            } else if (results[i].score === "Uncertain") {
+                ret.nrUncertain += 1;
             }
         }
 
@@ -38,6 +44,9 @@ const gptNrYes = () => {
 
 const totalEvaluationAverage = () => {
     let nrYesAnswers = 0;
+    let nrNoAnswers = 0;
+    let nrUncertainAnswers = 0;
+
     let totalNrAnswers = 0;
 
     if (props.application?.evaluation_answers?.length > 0) {
@@ -46,36 +55,64 @@ const totalEvaluationAverage = () => {
             let answers = JSON.parse(
                 props.application.evaluation_answers[i].answers
             );
+            console.log(answers);
             for (let j = 0; j < answers.length; j++) {
                 totalNrAnswers += 1;
                 if (answers[j] == "Yes") {
                     nrYesAnswers += 1;
+                } else if (answers[j] == "No") {
+                    nrNoAnswers += 1;
+                } else if (answers[j] == "Uncertain") {
+                    nrUncertainAnswers += 1;
                 }
             }
         }
     }
 
-    const gpt = gptNrYes();
+    const gpt = gptNrs();
     if (gpt) {
         nrYesAnswers += gpt.nrYes;
+        nrNoAnswers += gpt.nrNo;
+        nrUncertainAnswers += gpt.nrUncertain;
         totalNrAnswers += gpt.totalNrAnswers;
     }
 
     const score = parseInt((nrYesAnswers / totalNrAnswers) * 100);
-    return isNaN(score) ? null : score;
+    return {
+        score: isNaN(score) ? null : score,
+        yes: nrYesAnswers,
+        no: nrNoAnswers,
+        uncertain: nrUncertainAnswers,
+        total: totalNrAnswers,
+    };
 };
 </script>
 <template>
-    <div
-        v-if="totalEvaluationAverage() !== null"
-        class="h-12 w-12 rounded-full flex items-center justify-center text-white text-sm"
-        :class="{
-            'bg-red-500': totalEvaluationAverage() < 40,
-            'bg-orange-500':
-                totalEvaluationAverage() >= 40 && totalEvaluationAverage() < 70,
-            'bg-green-500': totalEvaluationAverage() >= 70,
-        }"
-    >
-        {{ totalEvaluationAverage() }}%
+    <div>
+        <div
+            v-if="totalEvaluationAverage() !== null"
+            class="h-12 w-12 rounded-full flex items-center justify-center text-white text-sm"
+            :class="{
+                'bg-red-500': totalEvaluationAverage().score < 40,
+                'bg-orange-500':
+                    totalEvaluationAverage().score >= 40 &&
+                    totalEvaluationAverage().score < 70,
+                'bg-green-500': totalEvaluationAverage().score >= 70,
+            }"
+        >
+            {{ totalEvaluationAverage().score }}%
+        </div>
+        <div
+            v-if="
+                totalEvaluationAverage().uncertain >=
+                totalEvaluationAverage().yes + totalEvaluationAverage().no
+            "
+            class="text-xs text-gray-500 items-center flex justify-center"
+        >
+            {{ totalEvaluationAverage().uncertain }} uncertain<span
+                v-if="totalEvaluationAverage().uncertain > 1"
+                >s</span
+            >
+        </div>
     </div>
 </template>
