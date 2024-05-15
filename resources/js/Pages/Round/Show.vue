0a0710fae63cd5f541b0ad1232d5953b48c8f2ae
Gerrit Giliomee <giliomeejg@gmail.com>
2024-05-15 08:12:04 +0200

    fix(href):

--- resources/js/Pages/Round/Show.vue
+++ resources/js/Pages/Round/Show.vue
@@ -659,6 +659,7 @@ const downloadReviewsCSV = async () => {
 
                         <Link
                             @click="downloadReviewsCSV"
+                            href="#"
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
                         >
                             Export user reviews .csv
