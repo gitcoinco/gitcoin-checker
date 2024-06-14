e62e7cceeed778f6440182541c4618531eddb079
Gerrit Giliomee <giliomeejg@gmail.com>
2024-06-14 15:09:50 +0200

    func(Analytics): Add cache.

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -14,48 +14,52 @@ class AnalyticsController extends Controller
     public function index()
     {
 
-        $roundsInThePastYear = Round::where('created_at', '>=', now()->subYear())
-            ->where('name', 'not like', '% test%')
-            ->where('name', 'not like', '%test %')
-            ->get(['id', 'uuid', 'name']);
+        $roundsInThePastYear = cache()->remember('rounds_in_the_past_year', 86400, function () {
+            return Round::where('created_at', '>=', now()->subYear())
+                ->where('name', 'not like', '% test%')
+                ->where('name', 'not like', '%test %')
+                ->get(['id', 'uuid', 'name']);
+        });
 
-        $stats = [
-            'rounds' => 0,
-            'applications' => 0,
-            'roundsEvaluatedByHumans' => 0,
-            'roundApplicationsEvaluatedByHumans' => 0,
-            'roundsEvaluatedByAI' => 0,
-            'roundApplicationsEvaluatedByAI' => 0,
-        ];
+        $stats = cache()->remember('analytics_stats', 86400, function () use ($roundsInThePastYear) {
+            $stats = [
+                'rounds' => 0,
+                'applications' => 0,
+                'roundsEvaluatedByHumans' => 0,
+                'roundApplicationsEvaluatedByHumans' => 0,
+                'roundsEvaluatedByAI' => 0,
+                'roundApplicationsEvaluatedByAI' => 0,
+            ];
 
-        foreach ($roundsInThePastYear as $round) {
-            $applicationsCount = RoundApplication::where('round_id', $round->id)->count();
-            $stats['rounds']++;
-            $stats['applications'] += $applicationsCount;
+            foreach ($roundsInThePastYear as $round) {
+                $applicationsCount = RoundApplication::where('round_id', $round->id)->count();
+                $stats['rounds']++;
+                $stats['applications'] += $applicationsCount;
 
-            $roundApplicationEvaluationAnswers = RoundApplicationEvaluationAnswers::where('round_id', $round->id)
-                ->count();
+                $roundApplicationEvaluationAnswers = RoundApplicationEvaluationAnswers::where('round_id', $round->id)
+                    ->count();
 
-            if ($roundApplicationEvaluationAnswers > 0) {
-                $stats['roundsEvaluatedByHumans']++;
+                if ($roundApplicationEvaluationAnswers > 0) {
+                    $stats['roundsEvaluatedByHumans']++;
+                }
+
+                $stats['roundApplicationsEvaluatedByHumans'] += $roundApplicationEvaluationAnswers;
+
+                $roundApplicationPromptResults = RoundApplicationPromptResult::select('application_id')
+                    ->where('round_id', $round->id)
+                    ->groupBy('application_id')
+                    ->havingRaw('COUNT(application_id) = 1')
+                    ->get()
+                    ->count();
+                if ($roundApplicationPromptResults > 0) {
+                    $stats['roundsEvaluatedByAI']++;
+                }
+
+                $stats['roundApplicationsEvaluatedByAI'] += $roundApplicationPromptResults;
             }
 
-            $stats['roundApplicationsEvaluatedByHumans'] += $roundApplicationEvaluationAnswers;
-
-
-            $roundApplicationPromptResults = RoundApplicationPromptResult::select('application_id')
-                ->where('round_id', $round->id)
-                ->groupBy('application_id')
-                ->havingRaw('COUNT(application_id) = 1')
-                ->get()
-                ->count();
-            if ($roundApplicationPromptResults > 0) {
-                $stats['roundsEvaluatedByAI']++;
-            }
-
-            $stats['roundApplicationsEvaluatedByAI'] += $roundApplicationPromptResults;
-        }
-
+            return $stats;
+        });
 
 
         return Inertia::render('Analytics/Index', [
